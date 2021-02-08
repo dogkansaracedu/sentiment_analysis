95d2528efc1bf1e24ce35cb413e36ce328897d57
dogkansaracedu <73716880+dogkansaracedu@users.noreply.github.com>
2021-02-08 10:06:29 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pickle
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import time
+
+
+app = Flask(__name__)
+CORS(app)
+
+# load the model
+loaded_model = pickle.load(open('/home/stajyer1/PycharmProjects/pythonProject/final_model.sav', 'rb'))
+# load the vectorizer
+loaded_vectorizer = pickle.load(open('/home/stajyer1/PycharmProjects/pythonProject/vector.pickel', 'rb'))
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+    content = request.get_json(silent=True)
+    pred = loaded_model.predict(loaded_vectorizer.transform([content['language']]))
+    if pred == 1:
+        retVal = "Pozitif"
+    else:
+        retVal = "Negatif"
+
+    # time.sleep(3)
+    response = jsonify({'result': retVal})
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
